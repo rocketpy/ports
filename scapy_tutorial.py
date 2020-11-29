7e990e9e2d7637db86ebef7674768e2470002118
rocketpy <dimitr.ushakov@gmail.com>
2020-11-29 22:51:35 +0200

    updated

--- scapy_tutorial.py
+++ scapy_tutorial.py
@@ -1,1 +1,9 @@
+#  Scapy is a Python program that enables the user to send, sniff and dissect and forge network packets.
+#  This capability allows construction of tools that can probe, scan or attack networks.
+
+#  DOCS: https://scapy.readthedocs.io/en/latest/introduction.html#about-scapy
+#  pypi: https://pypi.org/project/scapy/
+#  Scapy in 15 minutes:  https://github.com/secdev/scapy/blob/master/doc/notebooks/Scapy%20in%2015%20minutes.ipynb
+
+#  pip install scapy
 
