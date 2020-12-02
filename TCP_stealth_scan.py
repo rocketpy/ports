1dd124f7ecc2bbaa2ca541240d1d6fb19e19e946
rocketpy <dimitr.ushakov@gmail.com>
2020-12-02 22:48:11 +0200

    updated

--- TCP_stealth_scan.py
+++ TCP_stealth_scan.py
@@ -8,6 +8,19 @@ dst_ip = "10.0.0.1"
 src_port = RandShort()
 dst_port = 80
 
+stealth_scan_resp = sr1(IP(dst=dst_ip)/TCP(sport=src_port, dport=dst_port, flags="S"), timeout=10)
+
+if(str(type(stealth_scan_resp)) == "<type 'NoneType'>"):
+    print("Filtered")
+elif(stealth_scan_resp.haslayer(TCP)):
+    if(stealth_scan_resp.getlayer(TCP).flags == 0x12):
+        send_rst = sr(IP(dst=dst_ip)/TCP(sport=src_port, dport=dst_port, flags="R"), timeout=10)
+        print("Open")
+elif (stealth_scan_resp.getlayer(TCP).flags == 0x14):
+    print("Closed")
+elif(stealth_scan_resp.haslayer(ICMP)):
+    if(int(stealth_scan_resp.getlayer(ICMP).type)==3 and int(stealth_scan_resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
+        print("Filtered")
 
 """
 This technique is similar to the TCP connect scan. The client sends a TCP packet with the SYN flag set and the port number to connect to.
