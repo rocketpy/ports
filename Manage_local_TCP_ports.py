9dfedce47de96c6df95ec6434b7c58cd4adac898
rocketpy <dimitr.ushakov@gmail.com>
2021-05-03 22:11:25 +0300

    Update Manage_local_TCP_ports.py

--- Manage_local_TCP_ports.py
+++ Manage_local_TCP_ports.py
@@ -1,1 +1,12 @@
+#  port-for - Utility that helps with local TCP ports managment. It can find an unused TCP localhost port and remember the association.
 
+# PyPi: https://pypi.org/project/port-for/
+# pip install port-for
+
+import socket
+
+
+s = socket.socket()
+s.bind(("", 0))
+s.getsockname()
+# ('0.0.0.0', 54485)
